838523621d25f1b1752c536b88a5f99d6b1f7a5d
bonfs <matheusbonfim05@gmail.com>
2021-12-07 11:00:55 -0300

    RemoteAuthentication throws UnexpecedError on 404

--- test/data/usecases/remote_authentication_test.dart
+++ test/data/usecases/remote_authentication_test.dart
@@ -38,6 +38,15 @@ void main() {
   test('Should throw UnexpectedError if HttpClient returns 400', () async {
     when(httpClient.request(url: anyNamed('url'), method: anyNamed('method'), body: anyNamed('body')))
       .thenThrow(HttpError.badRequest);
+
+    final future = sut.auth(params);
+
+    expect(future, throwsA(DomainError.unexpected));
+  });
+
+  test('Should throw UnexpectedError if HttpClient returns 404', () async {
+    when(httpClient.request(url: anyNamed('url'), method: anyNamed('method'), body: anyNamed('body')))
+      .thenThrow(HttpError.notFound);
       
     final future = sut.auth(params);
 
