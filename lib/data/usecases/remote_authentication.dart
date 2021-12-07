9331832208a93961190e5d87fd70093e7727fcb7
bonfs <matheusbonfim05@gmail.com>
2021-12-07 09:54:49 -0300

    remoção de detalhes da API from domain to data

--- lib/data/usecases/remote_authentication.dart
+++ lib/data/usecases/remote_authentication.dart
@@ -6,11 +6,31 @@ class RemoteAuthentication {
   final String url;
 
   Future<void> auth(AuthenticationParams params) async {
-    await httpClient.request(url: url, method: 'post', body: params.toJson());
+    final body = RemoteAuthenticationParams.fromModel(params).toJson();
+    await httpClient.request(
+      url: url, 
+      method: 'post', 
+      body: body,
+    );
   }
 
   RemoteAuthentication({
     required this.httpClient,
     required this.url
   });
+}
+
+class RemoteAuthenticationParams {
+  final String email;
+  final String password;
+
+  RemoteAuthenticationParams({
+    required this.email, 
+    required this.password
+  });
+
+  factory RemoteAuthenticationParams.fromModel(AuthenticationParams params) =>
+    RemoteAuthenticationParams(email: params.email, password: params.password);
+
+  Map toJson() => { 'email': email, 'password': password };
 }
