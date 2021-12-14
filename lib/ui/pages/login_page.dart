1ebf104d9dfc7d8fbabc02043b5dad9a4a77ce6f
bonfs <matheusbonfim05@gmail.com>
2021-12-13 22:00:19 -0300

    add LoginPage

---
+++ lib/ui/pages/login_page.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              child: const Image(image: AssetImage('lib/ui/assets/logo.png')),
+            ),
+            Text('Login'.toUpperCase()),
+            Form(
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      labelText: 'Email',
+                      icon: Icon(Icons.email)
+                    ),
+                    keyboardType: TextInputType.emailAddress,
+                  ),
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      labelText: 'Senha',
+                      icon: Icon(Icons.lock)
+                    ),
+                    obscureText: true,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: Text('Entrar'.toUpperCase()),
+                  ),
+                  TextButton.icon(
+                    onPressed: () {},
+                    label: Text('Criar conta'),
+                    icon: Icon(Icons.person),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
