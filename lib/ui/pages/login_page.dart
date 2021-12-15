bd1fc60d6a401e3033343b4e7ab71aba5112089c
bonfs <matheusbonfim05@gmail.com>
2021-12-14 21:36:47 -0300

    add login page layout styles

--- lib/ui/pages/login_page.dart
+++ lib/ui/pages/login_page.dart
@@ -1,43 +1,52 @@
 import 'package:flutter/material.dart';
 
+import '../components/components.dart';
+
 class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Container(
-              child: const Image(image: AssetImage('lib/ui/assets/logo.png')),
-            ),
-            Text('Login'.toUpperCase()),
-            Form(
-              child: Column(
-                children: [
-                  TextFormField(
-                    decoration: const InputDecoration(
-                      labelText: 'Email',
-                      icon: Icon(Icons.email)
+            LoginHeader(),
+            const Headline1(text: 'Login'),
+            Padding(
+              padding: const EdgeInsets.all(32),
+              child: Form(
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration: InputDecoration(
+                        labelText: 'Email',
+                        icon: Icon(Icons.email, color: Theme.of(context).primaryColorLight)
+                      ),
+                      keyboardType: TextInputType.emailAddress,
                     ),
-                    keyboardType: TextInputType.emailAddress,
-                  ),
-                  TextFormField(
-                    decoration: const InputDecoration(
-                      labelText: 'Senha',
-                      icon: Icon(Icons.lock)
+                    Padding(
+                      padding: const EdgeInsets.only(top: 8, bottom: 32),
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          labelText: 'Senha',
+                          icon: Icon(Icons.lock, color: Theme.of(context).primaryColorLight)
+                        ),
+                        obscureText: true,
+                      ),
                     ),
-                    obscureText: true,
-                  ),
-                  ElevatedButton(
-                    onPressed: () {},
-                    child: Text('Entrar'.toUpperCase()),
-                  ),
-                  TextButton.icon(
-                    onPressed: () {},
-                    label: Text('Criar conta'),
-                    icon: Icon(Icons.person),
-                  ),
-                ],
+                    ElevatedButton(
+                      onPressed: () {},
+                      child: Text('Entrar'.toUpperCase()),
+                    ),
+                    TextButton.icon(
+                      onPressed: () {},
+                      label: const Text('Criar conta'),
+                      icon: const Icon(Icons.person),
+                    ),
+                  ],
+                ),
               ),
             )
           ],
