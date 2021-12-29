3998fb0ea372228e5152c89c0dc99d3c69a38c77
bonfs <matheusbonfim05@gmail.com>
2021-12-28 21:48:02 -0300

    move sut creation to setUp method

--- test/presentation/presenters/stream_login_presenter_test.dart
+++ test/presentation/presenters/stream_login_presenter_test.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:faker/faker.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
@@ -6,11 +8,18 @@ import 'package:test/test.dart';
 import 'stream_login_presenter_test.mocks.dart';
 
 abstract class Validation {
-  String? validate({required String field, required String value});
+  String validate({required String field, required String value});
+}
+
+class LoginState {
+  String emailError = '';
 }
 
 class StreamLoginPresenter {
   final Validation validation;
+  final _controller = StreamController<LoginState>.broadcast();
+
+  Stream<String> get emailErrorStream => _controller.stream.map((state) => state.emailError);
 
   StreamLoginPresenter({ required this.validation });
   
@@ -30,10 +39,19 @@ void main() {
     sut = StreamLoginPresenter(validation: validation);
     email = faker.internet.email();
   });
-  
+
   test('Should call validation with correct email', () {
     sut.validateEmail(email);
 
     verify(validation.validate(field: 'email', value: email)).called(1);
+  });
+
+  test('Should emit error if validations fails', () {
+    when(validation.validate(field: anyNamed('field'), value: anyNamed('value')))
+      .thenReturn('error');
+
+    expectLater(sut.emailErrorStream, emits('error'));
+
+    sut.validateEmail(email);
   });
 }
