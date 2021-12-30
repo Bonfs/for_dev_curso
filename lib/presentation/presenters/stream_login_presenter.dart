eb30e82f36f37f8620de436f8b7e1b5d0b1c31cd
bonfs <matheusbonfim05@gmail.com>
2021-12-29 22:24:13 -0300

    ensure isFormValid emits false if email is invalid

--- lib/presentation/presenters/stream_login_presenter.dart
+++ lib/presentation/presenters/stream_login_presenter.dart
@@ -8,6 +8,7 @@ class StreamLoginPresenter {
   final _state = LoginState();
 
   Stream<String> get emailErrorStream => _controller.stream.map((state) => state.emailError).distinct();
+  Stream<bool> get isFormValidStream => _controller.stream.map((state) => state.isFormValid).distinct();
 
   StreamLoginPresenter({ required this.validation });
   
@@ -19,4 +20,5 @@ class StreamLoginPresenter {
 
 class LoginState {
   String emailError = '';
+  bool get isFormValid => false;
 }
