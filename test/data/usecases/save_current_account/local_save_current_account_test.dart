ff321bac1408dc1e42683015379e0181451621d7
bonfs <matheusbonfim05@gmail.com>
2022-01-20 21:17:57 -0300

    move production code to the own files

--- test/data/usecases/save_current_account/local_save_current_account_test.dart
+++ test/data/usecases/save_current_account/local_save_current_account_test.dart
@@ -1,31 +1,14 @@
 import 'package:faker/faker.dart';
-import 'package:for_dev_curso/domain/helpers/helpers.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 
+import 'package:for_dev_curso/data/cache/cache.dart';
+import 'package:for_dev_curso/data/usecases/usecases.dart';
 import 'package:for_dev_curso/domain/entities/entities.dart';
-import 'package:for_dev_curso/domain/usecases/usecases.dart';
+import 'package:for_dev_curso/domain/helpers/helpers.dart';
+
 import './local_save_current_account_test.mocks.dart';
-
-class LocalSaveCurrentAccount implements SaveCurrentAccount {
-  final SaveSecureCacheStorage saveSecureCacheStorage;
-
-  LocalSaveCurrentAccount({required this.saveSecureCacheStorage});
-
-  @override
-  Future<void> save(AccountEntity entity) async {
-    try {
-      await saveSecureCacheStorage.saveSecure(key: 'token', value: entity.token);
-    } catch(error) {
-      throw DomainError.unexpected;
-    }
-  }
-}
-
-abstract class SaveSecureCacheStorage {
-  Future<void> saveSecure({required String key, required String value});
-}
 
 @GenerateMocks([], customMocks: [MockSpec<SaveSecureCacheStorage>(as: #SaveSecureCacheStorageSpy)])
 void main() {
